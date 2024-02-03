56110728469b09fdb93f785bbff3a6347ef8dfa0
Kálmán Ferenc <kalman.ferenc@akribis.hu>
2024-02-03 09:56:14 +0100

    Add more configurations

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "My first flake NixOS install";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-23.11";
+  };
+
+  outputs = {self, nixpkgs, ...}:
+  let
+    lib = nixpkgs.lib;
+  in {
+    nixosConfigurations = {
+      flake_mini_bios = lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ flake_mini_bios/configuration.nix ];
+      };
+      flake_mini = lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ flake_mini/configuration.nix ];
+      };
+      nixos = lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ nixos/configuration.nix ];
+      };
+    };
+  };
+}
