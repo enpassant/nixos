7aa5c14044c521d001489f72434e1e7707b88e75
Enpassant <enpassant.prog@gmail.com>
2025-01-03 23:10:59 +0100

    Add printing

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -123,7 +123,20 @@ in {
   };
 
   # Enable CUPS to print documents.
-  services.printing.enable = true;
+  services.printing = {
+    enable = true;
+    drivers = with pkgs; [ gutenprint canon-cups-ufr2 cups-filters ];
+    browsing = true;
+    browsedConf = ''
+BrowseDNSSDSubTypes _cups,_print
+BrowseLocalProtocols all
+BrowseRemoteProtocols all
+CreateIPPPrinterQueues All
+
+BrowseProtocols all
+    '';
+  };
+  
   services.avahi = {
     enable = true;
     nssmdns4 = true;
